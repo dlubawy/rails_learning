404b91fa042c690e7a1a2f3bbc88545a1987f79f
Andrew Lubawy <dlubawy@gmail.com>
2015-06-04 13:56:17 -0400

    Initial Commit

---
+++ blogger/app/controllers/tags_controller.rb
@@ -0,0 +1,17 @@
+class TagsController < ApplicationController
+  before_filter :require_login, only: [:destroy]
+
+  def index
+    @tags = Tag.all
+  end
+
+  def show
+    @tag = Tag.find(params[:id])
+  end
+
+  def destroy
+    @tag = Tag.destroy(params[:id])
+    flash.notice = "Tag '#{@tag.name}' Deleted!"
+    redirect_to tags_path
+  end
+end
